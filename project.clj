4829c1d459dac0bfd5d8a984653dca01a6fb6457
countgizmo <count.gizmo@gmail.com>
2017-11-01 15:56:35 +0700

    Printing the state for debug
    The only actual result of today's coding session is a number of functions that help me "see" the
    board. Should be useful for debugging.
    
    My algorithm approach was silly and didn't work. I was trying to be too smart. That's a no-go for
    flood fill approach. But I have drafted a new approach on paper. I will continue soon with first
    just doing the filling (without trying to be smart and applying additional logic).
    
    P.S. I've removed all the stuff from the project.clj file. That stuff was done a long time ago - I don't know what it is now. I will be doing a proper dependency setup when I'll need it. Wanna take this project one step at a time to learn and understand... how the sausage is made.

--- project.clj
+++ project.clj
@@ -2,37 +2,7 @@
   :description "Battle of Zots. It's your zots agains your enemy's"
 
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
-
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.48"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.omcljs/om "0.8.8"]
-                 [ring "1.4.0"]
-                 [compojure "1.4.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [om-sync "0.1.1"]
-                 [proto-repl "0.3.1"]
-                 [org.clojure/test.check "0.9.0"]
-                 [com.datomic/datomic-free "0.9.5130" :exclusions [joda-time]]]
-
-  :plugins [[lein-cljsbuild "1.1.0"]
-            [lein-figwheel "0.4.0"]]
-
-
-  :source-paths ["src/clj" "src/cljs"]
-  :resource-paths ["resources"]
-  :test-paths ["test" "src/test/clj"]
-  :clean-targets ^{:protect false} ["resources/public/js/out"
-                                    "resources/public/js/main.js"]
-
-  :figwheel {:ring-handler zots.core/handler}
-
-  :cljsbuild {:builds [{:id "dev"
-                        :source-paths ["src/clj" "src/cljs"]
-                        :figwheel true
-                        :compiler {:output-to "resources/public/js/main.js"
-                                   :output-dir "resources/public/js/out"
-                                   :main zots.core
-                                   :asset-path "js/out"
-                                   :optimizations :none
-                                   :source-map true}}]})
+  :dependencies
+   [[org.clojure/clojure "1.8.0"]
+    [proto-repl "0.3.1"]
+    [org.clojure/test.check "0.9.0"]])
--- src/clj/zots/board.clj
+++ src/clj/zots/board.clj
@@ -1,4 +1,4 @@
-(ns zots.board)
+(ns zots.board (:require [proto-repl.saved-values]))
 
 (def min-x 0)
 (def min-y 0)
@@ -22,25 +22,72 @@
         (> x max-x)
         (> y max-y))))
 
+(defn flood-cell?
+ [x y visited]
+ (and
+   (not (visited? x y visited))
+   (should-visit? x y)))
+
+(defn check-cell
+ [[x y] state]
+ (if (flood-cell? x y (:visited state))
+   (fill-flood x y state)
+   state))
+
+(defn row->str [r]
+ (reduce #(str %1 (-> %2 :player player->symbol)) "" r))
+
+(defn board->row [b]
+ (reduce #(str %1 (row->str %2) "\n") "" b))
+
+(defn print-board [state]
+ (print (board->row (get-in state [:board]))))
+
+(print-board state)
+
+
+
+; todo:
+; 1. Write flooding algorith only - use invisible ink :P
+; 2. Parse flooded state.
+; 3. Paint walls, change statuses accordingly to flooded state.
+
 (defn fill-flood
- [x y board visited]
- (println (str "Visiting " x " " y))
- (let [v (add-visited x y visited)]
-   (if (and (not (visited? (dec x) y v)) (should-visit? (dec x) y))
-     (fill-flood (dec x) y board v)
-     board)
-   (if (and (not (visited? (dec x) (dec y) v)) (should-visit? (dec x) (dec y)))
-     (fill-flood (dec x) (dec y) board v)
-     board)
-   (if (and (not (visited? (inc x) y v)) (should-visit? (inc x) y))
-     (fill-flood (inc x) y board v)
-     board)
-   (if (and (not (visited? (inc x) (inc y) v)) (should-visit? (inc x) (inc y)))
-     (fill-flood (inc x) (inc y) board v)
-     board)))
+ [x y state]
+ (let [new-visited (add-visited x y (:visited state))
+       new-state (parse-cell x y state)
+       north-coord [x (inc y)]
+       north-east-coord [(inc x) (inc y)]
+       east-coord [(inc x) y]
+       south-east-coord [(inc x) (dec y)]
+       south-coord [x (dec y)]
+       south-west-coord [(dec x) (dec y)]
+       west-coord [(dec x) y]
+       north-west-coord [(dec x) (dec y)]]
+   (->> (assoc-in new-state [:visited] new-visited)
+     (check-cell north-coord)
+     (check-cell north-east-coord)
+     (check-cell east-coord)
+     (check-cell south-east-coord)
+     (check-cell south-coord)
+     (check-cell south-west-coord)
+     (check-cell west-coord)
+     (check-cell north-west-coord))))
+
+(fill-flood 1 1 {:board simple-surround :visited [] :target [1 1]})
+
+(def test-state
+ {:board simple-surround
+  :target [1 0]
+  :visited []})
+
+(def tri-problem
+ [[{:y 0, :surrounded :false, :status :active, :player :red, :x 0}
+   {:y 0, :surrounded :false, :status :active, :player :blue, :x 1}
+   {:y 0, :surrounded :false, :status :active, :player :red, :x 2}]])
 
 (def simple-surround
- [[{:y 0, :surrounded :false, :status :active, :player :red, :x 0}
+ [[{:y 0, :surrounded :false, :status :active, :player :blue, :x 0}
    {:y 0, :surrounded :false, :status :active, :player :red, :x 1}
    {:y 0, :surrounded :false, :status :active, :player :red, :x 2}]
   [{:y 1, :surrounded :false, :status :active, :player :red, :x 0}
