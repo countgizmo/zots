4973683e8caf831ae89617691b04b2dbb6a07dae
countgizmo <count.gizmo@gmail.com>
2018-01-10 07:16:33 +0700

    Added tests for valid_move? function.
    Just something I had to do.

--- test/clj/zots/game_test.clj
+++ test/clj/zots/game_test.clj
@@ -67,18 +67,6 @@
                 (assoc :player :blue :surrounded false))]
    (is (not (game/cell-taken-by? :red cell)))))
 
-
-(def board-example
- [[{:x 0, :y 0, :surrounded false, :status :active, :player :none}
-   {:x 1, :y 0, :surrounded false, :status :wall, :player :red}
-   {:x 2, :y 0, :surrounded false, :status :active, :player :none}]
-  [{:x 0, :y 1, :surrounded false, :status :wall, :player :red}
-   {:x 1, :y 1, :surrounded true, :status :active, :player :blue}
-   {:x 2, :y 1, :surrounded false, :status :wall, :player :red}]
-  [{:x 0, :y 2, :surrounded false, :status :active, :player :none}
-   {:x 1, :y 2, :surrounded false, :status :wall, :player :red}
-   {:x 2, :y 2, :surrounded false, :status :active, :player :none}]])
-
 (def game-state
   (-> (game/new-game)
       (assoc-in [:board 0 1 :player] :red)
@@ -93,3 +81,11 @@
         red-score (game/calculate-score game-state :red)]
     (is (zero? blue-score))
     (is (= 1 red-score))))
+
+(deftest valid-move-checks
+ (let [g (game/new-game)
+       turn (:turn g)]
+   (is (game/valid-move? g {:turn turn :x 0 :y 0}))
+   (is (not (game/valid-move? g {:turn (game/enemy turn) :x 0 :y 0})))
+   (is (game/valid-move? g {:turn turn :x 10 :y 5}))
+   (is (not (game/valid-move? g {:turn turn :x -1 :y 0})))))
